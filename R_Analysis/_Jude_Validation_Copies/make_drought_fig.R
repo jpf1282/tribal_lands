11fc117597189e4202500d6e952eec1cab2db2e2
Katie McConnell <kathryn.mcconnell@yale.edu>
2020-11-20 12:45:10 -0700

    Update to fire and heat plots

--- R_Analysis/_Jude_Validation_Copies/make_drought_fig.R
+++ R_Analysis/_Jude_Validation_Copies/make_drought_fig.R
@@ -14,3 +14,51 @@ drought.plot <- ggplot(data_t1and2_long, aes(time, drt_median, colour = time)) +
   theme(legend.position = "none")
 
 ggsave(plot=drought.plot,filename = "drought_boxplot.pdf")
+
+
+
+# Katie's Added Plots -----------------------------------------------------
+library(ggbeeswarm)
+
+data_t1and2_long <- filter(data_long, tribe %in% tribes_time1and2_lst$tribe)
+
+# Heat Days Count
+heat.plot <- ggplot(data_t1and2_long, aes(time, h_100_hist, colour = time)) +
+  geom_boxplot(varwidth = T) +
+  geom_quasirandom(alpha = 1/10, varwidth = TRUE) +
+  scale_y_continuous(breaks = seq(0, 60, by=20), labels = c("0", "20", "40", "Days Above 100 F   60")) +
+  scale_colour_manual(values = c('#E0E022','#ff0000'),aesthetics = c("colour", "fill")) +
+  theme_minimal() +
+  xlab("") +
+  ylab("") +
+  scale_x_discrete(labels = c("time 1" = "Historical",
+                              "time 2" = "Present-day")) +
+  theme(legend.position = "none")
+
+# Wildfire Mean
+fire.mean.plot <- ggplot(data_t1and2_long, aes(time, fire_mean, colour = time)) +
+  geom_boxplot(varwidth = T) +
+  geom_quasirandom(alpha = 1/10, varwidth = TRUE) +
+  scale_y_continuous(breaks = seq(0, 4), labels = c("Low Wildfire Hazard Potential   0", "1", "2", "3", "High Wildfire Hazard Potential   4")) +
+  scale_colour_manual(values = c('#E0E022','#ff0000'),aesthetics = c("colour", "fill")) +
+  theme_minimal() +
+  xlab("") +
+  ylab("") +
+  scale_x_discrete(labels = c("time 1" = "Historical",
+                              "time 2" = "Present-day")) +
+  theme(legend.position = "none")
+
+# Wildfire Median
+fire.median.plot <- ggplot(data_t1and2_long, aes(time, fire_median, colour = time)) +
+  geom_boxplot(varwidth = T) +
+  geom_quasirandom(alpha = 1/10, varwidth = TRUE) +
+  scale_y_continuous(breaks = seq(0, 4), labels = c("Low Wildfire Hazard Potential   0", "1", "2", "3", "High Wildfire Hazard Potential   4")) +
+  scale_colour_manual(values = c('#E0E022','#ff0000'),aesthetics = c("colour", "fill")) +
+  theme_minimal() +
+  xlab("") +
+  ylab("") +
+  scale_x_discrete(labels = c("time 1" = "Historical",
+                              "time 2" = "Present-day")) +
+  theme(legend.position = "none")
+
+
